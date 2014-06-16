bac53bbfeab8ed49203c0ff2d0f9c9a4c93fdd02
Daniel Fullmer <daniel.fullmer@byu.edu>
2014-06-15 21:44:54 -0700

    Added command line arguments

--- main.go
+++ main.go
@@ -3,7 +3,10 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
@@ -11,14 +14,29 @@ import (
 	"github.com/nzbfs/go-nzbfs/nzbfs"
 )
 
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s [nzbdb] [mountpoint]\n", os.Args[0])
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
 func main() {
-	conn, err := fuse.Mount("mnt")
+	flag.Usage = usage
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		usage()
+	}
+	nzbdbDir := args[0]
+	mountDir := args[1]
+
+	conn, err := fuse.Mount(mountDir)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
-	err = fs.Serve(conn, &nzbfs.NzbFS{"nzbdb"})
+	err = fs.Serve(conn, &nzbfs.NzbFS{nzbdbDir})
 	if err != nil {
 		log.Fatal(err)
 	}
